7a2865e4dd9f35936e56f86f0bf9d1f02ae5730c
separatrixxx <nikitalochmatov@gmail.com>
2025-01-11 16:07:57 +0300

    add seo

--- locales/en.locale.ts
+++ locales/en.locale.ts
@@ -2,7 +2,8 @@ export const en = {
     locale: "en",
     language: "English",
     lang: "EN",
-    turboton: 'TurboTON',
+    turboton: "TurboTON",
+    seo_text: "TurboTON is a revolutionary community-driven memecoin built on the TON blockchain, seamlessly blending the transformative power of AI-generated art with blockchain innovation. With a total supply of 100 million tokens, evenly split between exchanges and community distribution through the TurboTON farming game, we ensure that all liquidity remains within the community without founder allocations, presales, token minting, or transaction taxes. Dive into the world of TurboTON Farm, a mobile farming game where players collect Stardust, boost their ranks, and compete for rewards, including TurboTON tokens, cash prizes, and Telegram Premium subscriptions. Unlock creativity with TurboNFTs and exclusive Telegram sticker packs inspired by stunning AI-generated art, while fostering collaboration and fun with your friends. Join TurboTON and be part of a vibrant, innovative ecosystem that empowers its community with cutting-edge AI art and engaging gameplay, showcasing the limitless potential of decentralized technology.",
     error404: "Page not found, 404 error",
     error500: "Server failure, error 500",
     header_links: {
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,11 +14,17 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>{setLocale(router.locale).turboton}</title>
-        <meta name='description' content={setLocale(router.locale).turboton} />
+        <meta name='description' content={setLocale(router.locale).seo_text} />
         <meta property='og:title' content={setLocale(router.locale).turboton} />
-        <meta property='og:description' content={setLocale(router.locale).turboton} />
+        <meta property='og:description' content={setLocale(router.locale).seo_text} />
         <meta charSet="utf-8" />
         <link rel="icon" href="/logo.svg" type="image/svg+xml" />
+        <link rel="icon" href="/favicon.svg" sizes="120x120" type="image/svg+xml" />
+        <link rel="icon" href="/favicon-48x48.ico" sizes="48x48" type="image/x-icon" />
+        <link rel="icon" href="/favicon-144x144.ico" sizes="144x144" type="image/x-icon" />
+        <link rel="icon" href="/favicon-192x192.ico" sizes="192x192" type="image/x-icon" />
+        <link rel="apple-touch-icon" href="/favicon-167x167.ico" sizes="167x167" type="image/x-icon" />
+        <link rel="apple-touch-icon" href="/favicon-180x180.ico" sizes="180x180" type="image/x-icon" />
       </Head>
       <Component {...pageProps} />
     </>
